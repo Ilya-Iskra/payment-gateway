6e6e125dfb729e051219d55f2a85f89ca4bd50ee
Ilya Iskra <ilya.iskra2020@gmail.com>
2024-05-20 10:44:25 +0500

    Add Centered layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,15 @@
 // import { useState } from "react";
 import BackgroundTexture from "/src/components/BackgroundTexture";
+import Centered from "../../layouts/Cetnered/Centered.layout";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <BackgroundTexture />
+      <Centered>
+        <div>text</div>
+      </Centered>
     </div>
   );
 }
---
+++ src/layouts/Cetnered/Centered.layout.jsx
@@ -0,0 +1,7 @@
+import "./Centered.css";
+
+function Centered({ children }) {
+  return <div className="centered-layout">{children}</div>;
+}
+
+export default Centered;
