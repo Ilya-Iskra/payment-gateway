49ac08d49dbbfbc225f8679ba5cedda54008b0a4
Ilya Iskra <ilya.iskra2020@gmail.com>
2024-05-26 11:35:05 +0500

    Test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/nim-gateway/",
+  // base: "/nim-gateway/",
   plugins: [react(), svgr()],
 });
