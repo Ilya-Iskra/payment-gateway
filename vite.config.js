7bff578ecd2bfd99e12036d47824ff050d50d0cf
Ilya Iskra <ilya.iskra2020@gmail.com>
2024-06-21 17:17:02 +0500

    Fix: top-level await

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/nim-gateway/",
-  plugins: [react(), svgr()],
+  plugins: [topLevelAwait(), react(), svgr()],
 });
