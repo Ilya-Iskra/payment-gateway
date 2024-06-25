e93c57e3cc73f153f8f74206a5442356621c96b3
Ilya Iskra <ilya.iskra2020@gmail.com>
2024-06-25 15:39:24 +0500

    Add: VITE_URL_PATH_PREFIX

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/nim-gateway/",
-  plugins: [topLevelAwait(), react(), svgr()],
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_URL_PATH_PREFIX || "/",
+    plugins: [topLevelAwait(), react(), svgr()],
+  };
 });
